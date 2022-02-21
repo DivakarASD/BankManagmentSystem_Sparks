855784c0b5372fd6287f9d93379f7e7e94860095
DivakarASD <53742945+DivakarASD@users.noreply.github.com>
2022-02-21 19:41:51 +0530

    Add files via upload

---
+++ sparks_1/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Shift Bank </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Shift Bank</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="transactions.php">Transfer History</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page"
+                            href="https://www.xe.com/currencyconverter/convert/?Amount=20&From=INR&To=EUR">Currency
+                            Convertor </a>
+                    </li>
+
+            </div>
+
+
+            </ul>
+
+        </div>
+        </div>
+    </nav>
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+
+
+    </div>
+
+    <section class="my-5">
+        <div class="py-5">
+            <h3 class="text-center">About Us</h3>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="images/img.png" class="img-fluid">
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h1>Shift Bank</h1>
+                    <p1>Shift bank is one of the world's largest financial institutions, offering a comprehensive range of banking, investing, asset management, and other financial and risk management products and services to individuals, small and middle-market enterprises, and major organizations. In the United States, the corporation has over 4,200 retail financial facilities, 16,000 ATMs, and award-winning digital banking with roughly 41 million active users, including approximately 33 million mobile users, servicing approximately 67 million consumer and small business clients. Shift bank is a global leader in wealth management, corporate and investment banking, and trading across a diverse range of asset classes, servicing enterprises, governments, organizations, and people worldwide. Through a spectrum of innovative, easy-to-use online products and services, Shift bank provides industry-leading support to nearly 3 million small business families. Clients are served through the company's activities in the United States, its territories, and over 35 other countries. The New York Shares Exchange lists the stock of Shift Bank Corporation (NYSE: BAC).</p1>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+    <section class="my-5">
+        <div class="py-5">
+            <h3 class="text-center">Disclaimer</h3>
+            <p1 style="text-align: center;">This website is the outcome of a intern project, and does not
+                necessarily represent the views of any organisation or any other individuals referenced or
+                acknowledged within the website. Anyone may reproduce, distribute, adapt, translate the content on the
+                website, without explicit permission, provided that the content is accompanied by an acknowledgement
+                that Shift Bank website is the source and that it is clearly indicated if changes were made to the
+                original content.</p1>
+        </div>
+
+
+
+
+</body>
+
+</html>
